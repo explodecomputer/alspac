2b975096830cbd58c71b0a0ce482306468335f2e
Gibran Hemani <explodecomputer@gmail.com>
2018-10-11 19:25:03 +0100

    working

--- inst/collate.r
+++ inst/collate.r
@@ -9,8 +9,8 @@ out <- lapply(commandArgs(T), function(x)
 }) %>% bind_rows
 
 Current <- subset(out, grepl("^/Current", path), select=-c(path))
-save(Current, file="Current.RData")
+save(Current, file="../data/Current.RData")
 
 Useful_data <- subset(out, grepl("^/Useful_data", path), select=-c(path))
-save(Useful_data, file="Useful_data.RData")
+save(Useful_data, file="../data/Useful_data.RData")
 
--- inst/process_dta.r
+++ inst/process_dta.r
@@ -1,6 +1,53 @@
 suppressMessages(suppressWarnings(library(dplyr)))
 suppressMessages(suppressWarnings(library(readstata13)))
 
+countCharOccurrences <- function(char, s)
+{
+	s2 <- gsub(char,"",s)
+	return (nchar(s) - nchar(s2))
+}
+
+
+trimWhitespace <- function(x)
+{
+	if(is.numeric(x))
+	{
+		return(x)
+	}
+	flag <- is.factor(x)
+	x <- gsub("^\\s+|\\s+$", "", x)
+	if(flag) 
+	{
+		return(as.factor(x))
+	} else {
+		return(x)
+	}
+}
+
+
+createFileTable <- function(fls, alspacdir)
+{
+	fls_dn <- dirname(fls)
+	fls_bn <- basename(fls)
+	fls_n <- gsub(".dta", "", fls_bn, ignore.case=T)
+	fls_d <- gsub(alspacdir, "", fls_dn)
+	fls_d <- gsub("^/", "", fls_d)
+	nfield <- max(countCharOccurrences("/", fls_d)) + 1
+	stopifnot(! any(duplicated(fls_n)))
+
+	sp <- strsplit(fls_d, split="/")
+	sp <- lapply(sp, function(x) {
+		y <- rep(NA, nfield)
+		y[1:length(x)] <- x
+		return(y)
+	})
+	dat <- data.frame(do.call("rbind", sp), stringsAsFactors=FALSE)
+	names(dat) <- paste("cat", 1:nfield, sep="")
+	dat$obj <- fls_bn
+	dat$path <- fls_d
+	return(dat)
+}
+
 process_dta <- function(fn)
 {
 	temp <- suppressWarnings(read.dta13(fn))
@@ -20,7 +67,10 @@ dta <- args[1]
 out <- args[2]
 alspacdir <- args[3]
 
-dat <- process_dta(dta)
-dat$path <- gsub(alspacdir, "", dta)
+dat <- merge(
+	process_dta(dta),
+	createFileTable(dta, alspacdir),
+	by="obj"
+)
 
 save(dat, file=out)
