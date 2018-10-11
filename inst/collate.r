7676323286b71a70b67d14f7e35cb813a99539f2
Gibran Hemani <explodecomputer@gmail.com>
2018-10-11 21:08:35 +0100

    upd

--- inst/collate.r
+++ inst/collate.r
@@ -8,9 +8,9 @@ out <- lapply(commandArgs(T), function(x)
 	return(dat)
 }) %>% bind_rows
 
-Current <- subset(out, cat1 == "Current", select=-c(path))
-save(Current, file="../data/Current.RData")
+current <- subset(out, cat1 == "Current", select=-c(path))
+save(current, file="../data/Current.RData")
 
-Useful_data <- subset(out, cat1 == "Useful_data", select=-c(path))
-save(Useful_data, file="../data/Useful_data.RData")
+useful <- subset(out, cat1 == "Useful_data", select=-c(path))
+save(useful, file="../data/Useful_data.RData")
 
