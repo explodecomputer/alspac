f183e78fdb111c011d8a0d2b5e6908aa70b994f3
Gibran Hemani <explodecomputer@gmail.com>
2019-06-19 08:47:29 +0100

    added test for data dictionary update

---
+++ inst/test.R
@@ -0,0 +1,19 @@
+if(! "devtools" %in% installed.packages()[,"Package"])
+{
+	install.packages("devtools")
+}
+
+devtools::load_all()
+
+data(current)
+data(useful)
+
+# Get random variables
+cvars <- sample(1:nrow(current), 3)
+uvars <- sample(1:nrow(useful), 3)
+
+cdat <- extractVars(current[cvars,])
+udat <- extractVars(useful[uvars,])
+
+str(cdat)
+str(udat)
