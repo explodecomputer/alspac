4328f1a0711b8563289b4a36826464a0575850ff
perishky <matthewsuderman@hotmail.com>
2021-08-10 15:25:15 +0100

    fixes docker bug

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -7,4 +7,4 @@ docker build --build-arg WHEN=2020-06-22 -t alspac-r-test .
 
 ## run the tests
 CURRENTDIR=`pwd`
-docker run -v $ALSPACDIR:/home/alspac -v $CURRENTDIR:/home/example alspac-r-test "cd /home/example; make all"
+docker run -v $ALSPACDIR:/home/alspac -v $CURRENTDIR:/home/example alspac-r-test /bin/bash -c "cd /home/example; make all"
