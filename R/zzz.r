f1a567971ffbcad0d4e5eba2246e2803567e2794
Gibran Hemani <explodecomputer@gmail.com>
2023-12-04 10:30:05 +0000

    added method to check if installed version is the latest version by comparing against github

--- R/zzz.r
+++ R/zzz.r
@@ -4,10 +4,24 @@
     message("This may lead to slightly different formatting compared to previously.")
     message("See also the new createDictionary function.")
 
+    a <- suppressWarnings(try(readLines("https://raw.githubusercontent.com/explodecomputer/alspac/master/DESCRIPTION"), silent=TRUE))
+
+    if(!inherits(a, 'try-error'))
+    {
+        latest <- gsub("Version: ", "", a[grep("Version", a)])
+        current = utils::packageDescription('alspac')
+
+        test <- utils::compareVersion(latest, current$Version)
+        if(test == 1)
+        {
+            packageStartupMessage("\nWarning:\nYou are running an old version of the R/alspac package.\n",
+                "This version:   ", current$Version, "\n",
+                "Latest version: ", latest, "\n",
+                "Please consider updating using remotes::install_github('explodecomputer/alspac')")
+        }
+    }
+
     loadDictionaries()
     
     tryCatch(setDataDir(), error=function(e) print(e$message))
 }
-
-
-
