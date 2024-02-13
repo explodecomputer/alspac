2432e6e8f7a2b7483ff6157a6663634d329220fd
Matt Hardcastle <matt.hardcastle23@gmail.com>
2024-02-13 13:55:47 +0000

    stopifnot plus waitingroom

--- R/dictionary.r
+++ R/dictionary.r
@@ -116,7 +116,7 @@ updateDictionaries <- function() {
 #' @export
 #' @return Data frame dictionary listing available variables.
 createDictionary <- function(datadir="Current", name=NULL, quick=F, sourcesFile = "sources.csv") {
-  stopifnot(datadir %in% c("Current"))
+  stopifnot(datadir %in% c("Current", "../DataBuddy/DataRequests/Waiting Room"))
   
   alspacdir <- options()$alspac_data_dir
   datadir <- file.path(alspacdir, datadir)
