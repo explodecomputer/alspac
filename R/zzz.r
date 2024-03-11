52f910b363edacb559d5cc23029c68a8bff6ec5e
Tom Palmer <remlapmot@hotmail.com>
2024-03-11 18:03:51 +0000

    Add utils namespace qualifier

--- R/zzz.r
+++ R/zzz.r
@@ -1,5 +1,5 @@
 .onLoad <- function(libname = find.package("alspac"), pkgname = "alspac") {
-    message("R/alspac version ", packageVersion("alspac"))
+    message("R/alspac version ", utils::packageVersion("alspac"))
     message("We have made several changes to how the extractVars function works.")
     message("This may lead to slightly different formatting compared to previously.")
     message("See also the new createDictionary function.")
