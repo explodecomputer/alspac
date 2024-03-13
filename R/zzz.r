a4c2f24dcda93efb1de96e23c4f911861b852d9e
Tom Palmer <remlapmot@hotmail.com>
2024-03-13 12:00:38 +0000

    Move some start up messages into .onAttach()

--- R/zzz.r
+++ R/zzz.r
@@ -1,13 +1,11 @@
-.onLoad <- function(libname = find.package("alspac"), pkgname = "alspac") {
-    message("R/alspac version ", utils::packageVersion("alspac"))
-    message("We have made several changes to how the extractVars function works.")
-    message("This may lead to slightly different formatting compared to previously.")
-    message("See also the new createDictionary function.")
-
-    loadDictionaries()
-    
-    tryCatch(setDataDir(), error=function(e) print(e$message))
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("R/alspac version ", utils::packageVersion("alspac"), ".\n",
+                        "We have made several changes to how the extractVars function works.\n",
+                        "This may lead to slightly different formatting compared to previously.\n",
+                        "See also the new createDictionary function.")
 }
 
-
-
+.onLoad <- function(libname, pkgname) {
+    loadDictionaries()
+    tryCatch(setDataDir(), error=function(e) print(e$message))
+}
