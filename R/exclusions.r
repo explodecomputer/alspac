a9fe5693638ccb18269d60e6dee8177ad7ac7852
Matt Hardcastle <matt.hardcastle23@gmail.com>
2024-03-26 14:04:20 +0000

    exclusions in_obj exception

--- R/exclusions.r
+++ R/exclusions.r
@@ -58,8 +58,8 @@ removeExclusions <- function(x, dictionary) {
       "in_phase2",
       "in_phase3",
       "in_phase4",
-      "tripquad"
-      #colnames(x)[grepl("^in_obj_", colnames(x))]
+      "tripquad",
+      colnames(x)[grepl("^in_obj_", colnames(x))]
       )
     
     #check that all variables in x are also in dictionary
