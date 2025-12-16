c3d93c66abc4d7b381632461165fc3b49b46592a
Tom Palmer <remlapmot@hotmail.com>
2025-12-16 10:14:07 +0000

    Use paste() within packageStartupMessage

--- R/zzz.r
+++ R/zzz.r
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("R/alspac version ", utils::packageVersion("alspac"), ".\n",
+  packageStartupMessage(paste("R/alspac version ", utils::packageVersion("alspac"), ".\n",
                         "We have made several changes to how the extractVars function works.\n",
                         "This may lead to slightly different formatting compared to previously.\n",
-                        "See also the new createDictionary function.")
+                        "See also the new createDictionary function."))
 }
 
 .onLoad <- function(libname, pkgname) {
